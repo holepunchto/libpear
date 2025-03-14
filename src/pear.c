00c3ccdacdb706954525749cd4dc158b934eb5b6
butera-simone <buterasimone@proton.me>
2025-03-14 12:32:34 -0600

    first attempt

--- src/pear.c
+++ src/pear.c
@@ -230,6 +230,27 @@ pear_launch(int argc, char *argv[], pear_id_t key, const char *name) {
 
   pear__path = NULL; // Default platform directory
 
+  #if defined(APPLING_OS_LINUX)
+  if (getenv("SNAP_USER_COMMON") != NULL) {
+    appling_path_t snap_path;
+    size_t snap_path_len = sizeof(appling_path_t);
+
+    err = path_join(
+      (const char *[]) {
+        getenv("SNAP_USER_COMMON"),
+        "pear",
+        NULL
+      },
+      snap_path,
+      &snap_path_len,
+      path_behavior_system
+    );
+    assert(err == 0);
+
+    pear__path = strdup(snap_path);
+  }
+  #endif
+
   err = appling_lock(uv_default_loop(), &pear__lock, pear__path, pear__on_lock);
   assert(err == 0);
 
