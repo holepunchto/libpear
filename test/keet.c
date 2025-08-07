40280c04f0e6ed7ecbd2d06500ff358ab5a3d942
Andrei Regiani <andrei.cpp@gmail.com>
2025-08-07 18:42:23 +0800

    pear link with key instead of alias

--- test/keet.c
+++ test/keet.c
@@ -4,7 +4,7 @@ int
 main(int argc, char *argv[]) {
   static pear_id_t id = "keet";
   static const char *name = "keet";
-  static const char *link = "pear://keet";
+  static const char *link = "pear://ma9zo8zmfat3ih314mne1q47shrfho1odyfm9methgnn7b866w6y";
 
   return pear_launch(argc, argv, id, name, link);
 }
