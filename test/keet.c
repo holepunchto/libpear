5566806aa66a3c20d377086344a5d12a9535e3a7
Andrei Regiani <andrei.cpp@gmail.com>
2025-08-19 19:36:32 +0800

    LINK

--- test/keet.c
+++ test/keet.c
@@ -4,7 +4,7 @@ int
 main(int argc, char *argv[]) {
   static pear_id_t id = "keet";
   static const char *name = "keet";
-  static const char *link = "pear://ma9zo8zmfat3ih314mne1q47shrfho1odyfm9methgnn7b866w6y";
+  static const char *link = "pear://oeeoz3w6fjjt7bym3ndpa6hhicm8f8naxyk11z4iypeoupn6jzpo";
 
   return pear_launch(argc, argv, id, name, link);
 }
